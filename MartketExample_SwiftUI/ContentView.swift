5bb4f72b86a1b5c250275cb8fbde19d80d2d4936
KimWooSung <kipsong133@gmail.com>
2022-01-01 13:49:50 +0900

    Initial Commit

---
+++ MartketExample_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MartketExample_SwiftUI
+//
+//  Created by 김우성 on 2022/01/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MartketExample_SwiftUI/MartketExample_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MartketExample_SwiftUIApp.swift
+//  MartketExample_SwiftUI
+//
+//  Created by 김우성 on 2022/01/01.
+//
+
+import SwiftUI
+
+@main
+struct MartketExample_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
